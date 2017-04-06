cd81e71bce98b937ccaf9ef1c4720118d097765a
kczapla <k.czapla68@gmail.com>
2017-04-06 11:04:46 +0200

    small changes

--- bash_profile.sh
+++ bash_profile.sh
@@ -3,12 +3,12 @@ MACOS_NVIM_PATH="/opt/local/bin/nvim"
 CURRENT_EDITOR_PATH=""
 if [[ -f $MACOS_NVIM_PATH ]]; then
     CURRENT_EDITOR_PATH=$MACOS_NVIM_PATH
-    alias nv=$MACOS_NVIM_PATH
 else
     echo "NeoVim not found. Using ordinary Vim."
     CURRENT_EDITOR_PATH=vim
-    alias nv=vim
 fi
+export EDITOR=$CURRENT_EDITOR_PATH
+alias nv=$CURRENT_EDITOR_PATH
 
 FCEDIT=$CURRENT_EDITOR_PATH
 # Disable log off -> ctrl-d
@@ -17,3 +17,6 @@ set -o ignoreeof
 set -o nounset
 # cdable vars 
 shopt -s cdable_vars
+
+# Midnigth Commander
+alias mc='LANG=en_EN.UTF-8 mc'
