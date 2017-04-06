391435d8e4200684ee870eedd27a9bbb3b43c184
kczapla <k.czapla68@gmail.com>
2017-04-06 11:57:54 +0200

    small fixes

--- install.sh
+++ install.sh
@@ -14,14 +14,3 @@ if [[ -e $HOMEUSER_BASH_PROFILE_PATH ]]; then
 fi
 echo "Creating ln -s $LOCAL_BASH_PROFILE_PATH $HOMEUSER_BASH_PROFILE_PATH."
 ln -s $LOCAL_BASH_PROFILE_PATH $HOMEUSER_BASH_PROFILE_PATH
-
-echo ""
-
-echo "bash_profile path: $LOCAL_BASH_PROFILE_PATH"
-echo "bash_profile path: $path $HOMEUSER_BASH_RC_PATH"
-if [[ -e $HOMEUSER_BASH_RC_PATH ]]; then
-	echo "$HOMEUSER_BASH_RC_PATH exists. Deleting."
-	rm -f $HOMEUSER_BASH_RC_PATH
-fi
-echo "Creating ln -s $LOCAL_BASH_PROFILE_PATH $HOMEUSER_BASH_RC_PATH."
-ln -s $LOCAL_BASH_PROFILE_PATH $HOMEUSER_BASH_RC_PATH
